e467cd2f2b2ce092d3528890e388dea5adba1622
gufronnakaaw <gufronnakaaw@gmail.com>
2026-05-20 20:53:03 +0700

    feat: add jwt module & swagger module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './common/services/prisma.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '6h',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { json, urlencoded } from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { urlencoded } from 'express';
 import { AppModule } from './app.module';
 import { GlobalException } from './common/exceptions/global.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.use(json({ limit: '5mb' }));
+  const config = new DocumentBuilder()
+    .setTitle('PPMI Workflow API Documentation')
+    .setDescription('The PPMI Workflow API documentation')
+    .setVersion('1.0')
+    .addTag('PPMI')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   app.use(urlencoded({ extended: true, limit: '5mb' }));
   app.useGlobalFilters(new GlobalException(app.get(HttpAdapterHost)));
   await app.listen(process.env.PORT || 3000);
